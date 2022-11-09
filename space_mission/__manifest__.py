416a154123850b8b07b1a90a0d0df8600c491639
Juan Daniel Perez <juandanielp24@gmail.com>
2022-11-09 17:47:15 +0000

    [ADD] Space Mission Module

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Space Mission',
+    'summary': """Space Mission App""",
+    'description': """
+        Space Mission module that allows organizing the logistics of trips to the moon
+    """,
+    'author': 'Juan Daniel Perez',
+    'website': 'https://github.com/juandanielp24',
+    'category': 'Training',
+    'version': '0.1',
+    'license': 'OPL-1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
